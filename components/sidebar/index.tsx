6f8d295e9b05fad95636e5acda453437da432875
Farshad Hatami <farshad.hatami95@gmail.com>
2024-05-07 15:17:37 +0330

    add authentication button to sidebar

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,16 +1,16 @@
-import AuthButtons from './auth-buttons';
+import AuthButton from '../auth/auth-button';
 import Logo from './logo';
 import Navbar from './navbar';
 import UserButton from './user-button';
 
 export default function SideBar() {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
 
   return (
     <div className="sticky flex h-screen w-56 flex-col justify-between py-12">
       <Logo />
       <Navbar />
-      {isUserLoggedIn ? <UserButton /> : <AuthButtons />}
+      {isUserLoggedIn ? <UserButton /> : <AuthButton />}
     </div>
   );
 }
