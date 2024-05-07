6c85cde3c39976ca60de773d7a0558f18c963be0
Farshad Hatami <farshad.hatami95@gmail.com>
2024-05-07 10:53:37 +0330

    add header  component to home page

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,3 +1,9 @@
+import Header from '@/components/header';
+
 export default function Home() {
-  return <div className="">Home</div>;
+  return (
+    <main>
+      <Header label="Home" />
+    </main>
+  );
 }
