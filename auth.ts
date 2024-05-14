af90c753e48893b6e341e0e1817ec9780cbdfbea
Farshad Hatami <farshad.hatami95@gmail.com>
2024-05-15 01:40:31 +0330

    migrate to bcryptjs from bcrypt

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import Google from 'next-auth/providers/google';
@@ -53,5 +53,5 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       },
     }),
   ],
-  debug: true,
+  debug: process.env.NODE_ENV !== 'production',
 });
